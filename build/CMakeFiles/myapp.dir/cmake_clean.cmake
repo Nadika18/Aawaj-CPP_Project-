866cdc439ee00836ffa6bc3ffed86f1717927c64
Nadika <nadikapoudel51@gmail.com>
2021-07-08 20:13:52 +0545

    Cmake as project management tool

---
+++ build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/src/main.cpp.obj"
+  "CMakeFiles/myapp.dir/src/main.cpp.obj.d"
+  "libmyapp.dll.a"
+  "myapp.exe"
+  "myapp.exe.manifest"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
